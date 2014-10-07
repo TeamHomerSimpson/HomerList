5cd92dbfbbc28fc1a751ec2ba225dd249bb9afdc
achoraev <audulin@gmail.com>
2014-10-07 15:43:52 +0300

    initial project

---
+++ gen/android/homersimpson/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package android.homersimpson;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/android/homersimpson/SecondScreen.java
@@ -0,0 +1,14 @@
+package android.homersimpson;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SecondScreen extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+	}
+
+}
