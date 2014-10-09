3c6a2bd05472ae29140dc077d9c9ca8c3ce6b91b
achoraev <audulin@gmail.com>
2014-10-09 13:24:30 +0300

    some UI

--- gen/android/homerlist/R.java
+++ gen/android/homerlist/R.java
@@ -25,20 +25,36 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080001;
-        public static final int button1=0x7f080000;
+        public static final int action_settings=0x7f080009;
+        public static final int addPicture=0x7f080004;
+        public static final int createNote=0x7f080005;
+        public static final int footer=0x7f080006;
+        public static final int header=0x7f080000;
+        public static final int linearLayout1=0x7f080001;
+        public static final int listView1=0x7f080008;
+        public static final int pager=0x7f080007;
+        public static final int submitNote=0x7f080003;
+        public static final int userInput=0x7f080002;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int second_screen=0x7f030001;
+        public static final int third_screen=0x7f030002;
     }
     public static final class menu {
         public static final int main=0x7f070000;
     }
     public static final class string {
         public static final int action_settings=0x7f050002;
+        public static final int add_pictire=0x7f050007;
         public static final int app_name=0x7f050000;
         public static final int button=0x7f050003;
+        public static final int create_post=0x7f050008;
+        public static final int created_by_team_homersimpson=0x7f050009;
         public static final int hello_world=0x7f050001;
+        public static final int homer_list=0x7f050004;
+        public static final int submit_post=0x7f050006;
+        public static final int username=0x7f050005;
     }
     public static final class style {
         /** 
--- src/android/homerlist/MainActivity.java
+++ src/android/homerlist/MainActivity.java
@@ -2,33 +2,50 @@ package android.homerlist;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements OnClickListener {
+
+	String username;
+	String note;
+	Button createNote;
+	EditText userInput, submitNote;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		userInput = (EditText) findViewById(R.id.userInput);
+		submitNote = (EditText) findViewById(R.id.submitNote);
+		createNote = (Button) findViewById(R.id.createNote);
+
+		createNote.setOnClickListener(this);
+
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
+	public void onClick(View v) {
+		note = submitNote.getText().toString();
+		username = userInput.getText().toString();
+		
+		if (v.getId() == R.id.createNote) {
+			if (note == "" || note == null) {
+				Toast.makeText(this, "You must write something!!!",
+						Toast.LENGTH_LONG).show();
+			} else if (username == "" || username == null) {
+				Toast.makeText(this, "You must choose username!!!",
+						Toast.LENGTH_LONG).show();
+			} else {
+				Toast.makeText(this, "Welcome " + username, Toast.LENGTH_SHORT)
+						.show();
+				Toast.makeText(this, submitNote.getText(), Toast.LENGTH_LONG)
+						.show();
+			}
 		}
-		return super.onOptionsItemSelected(item);
+
 	}
 }
