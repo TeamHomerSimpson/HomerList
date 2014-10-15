77028fba219272e29cc604b0b71aefd3c36bb658
achoraev <audulin@gmail.com>
2014-10-16 00:41:39 +0300

    final for day

--- src/android/homerlist/MainActivity.java
+++ src/android/homerlist/MainActivity.java
@@ -26,10 +26,12 @@ public class MainActivity extends Activity implements OnClickListener {
 	// for camera
 	private ImageView mView;
 	
+	Intent intent;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);		
+		setContentView(R.layout.activity_main);
 
 		userInput = (EditText) findViewById(R.id.userInput);
 		submitNote = (EditText) findViewById(R.id.submitNote);
@@ -42,23 +44,25 @@ public class MainActivity extends Activity implements OnClickListener {
 		// select location button
 		selectLocationBtn = (Button) findViewById(R.id.selectLocationBtn);
 		selectLocationBtn.setOnClickListener(this);
-	}
-	
-	private void startCamera() {
-		Intent camera = new Intent(
-		        android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
-		this.startActivityForResult(camera, 100);
+		// touch
+
 	}
 
+	private void startCamera() {
+		Intent camera = new Intent(
+				android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+		this.startActivityForResult(camera, 100);
+	}	
+
 	public void onClick(View v) {
-		
-		try {			
+
+		try {
 			if (v.getId() == R.id.selectLocationBtn) {
-				final Intent locationScreenIntent = new Intent(this, LocationActivity.class);
+				final Intent locationScreenIntent = new Intent(this,
+						LocationActivity.class);
 				startActivity(locationScreenIntent);
-			}			
-			else if (v.getId() == R.id.createNote) {
-				final Intent intent = new Intent(this, SecondScreen.class);
+			} else if (v.getId() == R.id.createNote) {
+				intent = new Intent(this, SecondScreen.class);
 				username = userInput.getText().toString();
 				note = submitNote.getText().toString();
 
@@ -66,35 +70,35 @@ public class MainActivity extends Activity implements OnClickListener {
 					if (v.getId() == R.id.createNote) {
 						intent.putExtra("username", username);
 						intent.putExtra("post", note);
-						Toast.makeText(this, "Welcome " + username, Toast.LENGTH_SHORT)
-								.show();
+						Toast.makeText(this, "Welcome " + username,
+								Toast.LENGTH_SHORT).show();
 
 						startActivity(intent);
 					}
 				}
 			} else if (v.getId() == R.id.addPicture) {
 				// start camera
-				startCamera();						
+				startCamera();
 			}
-			
+
 		} catch (Exception e) {
 			// TODO: handle exception
 		}
-	}
+	}	
 
 	@Override
-	protected void onActivityResult(int requestCode, int resultCode, Intent data) {		
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 		super.onActivityResult(requestCode, resultCode, data);
 		if (resultCode == RESULT_OK) {
-			mView = (ImageView) findViewById(R.id.imageView1);	
-			Bundle extras = data.getExtras();			
+			mView = (ImageView) findViewById(R.id.imageView1);
+			Bundle extras = data.getExtras();
 			Bitmap photo = (Bitmap) extras.get("data");
 			mView.setImageBitmap(photo);
 		}
 	}
 
 	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {		
+	public boolean onCreateOptionsMenu(Menu menu) {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return super.onCreateOptionsMenu(menu);
 	}
