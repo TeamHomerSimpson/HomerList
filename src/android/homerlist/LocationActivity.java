a8147c414e1dca34f3062c18087b5f73b184fc7a
Tsvetan Manev <tsvetanmanev@gmail.com>
2014-10-17 01:19:05 +0300

    Implemented map zoom-in to current location
    + dragable marker to select location

--- src/android/homerlist/LocationActivity.java
+++ src/android/homerlist/LocationActivity.java
@@ -1,45 +1,107 @@
 package android.homerlist;
 
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.GoogleMap.OnMarkerDragListener;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.provider.SyncStateContract.Constants;
+import android.util.Log;
 
-public class LocationActivity extends Activity {
+public class LocationActivity extends Activity implements LocationListener, OnMarkerDragListener{
 
-	private static final LatLng SYDNEY = new LatLng(-33.88,151.21);
-	private static final LatLng MOUNTAIN_VIEW = new LatLng(37.4, -122.1);
 	private GoogleMap map;
-	
+	private LocationManager locationManager;
+	private static final long MIN_TIME = 400;
+	private static final float MIN_DISTANCE = 1000;
+
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {	
+	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.location_activity);
-		
-		map = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
 
-		
-		map.moveCamera(CameraUpdateFactory.newLatLngZoom(SYDNEY, 15));
+		map = ((MapFragment) getFragmentManager().findFragmentById(R.id.map))
+				.getMap();
 
-		// Zoom in, animating the camera.
-		map.animateCamera(CameraUpdateFactory.zoomIn());
+		locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+	    locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, MIN_TIME, MIN_DISTANCE, this); //You can also use LocationManager.GPS_PROVIDER and LocationManager.PASSIVE_PROVIDER        
+	
+	    map.setOnMarkerDragListener(this);
+	}
 
-		// Zoom out to zoom level 10, animating with a duration of 2 seconds.
-		map.animateCamera(CameraUpdateFactory.zoomTo(10), 2000, null);
+	@Override
+	public void onLocationChanged(Location location) {
+		LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+	    CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(latLng, 14);
+	    map.animateCamera(cameraUpdate);
+	    locationManager.removeUpdates(this);
+	    
+	    Marker userMarker = map.addMarker(new MarkerOptions()
+	                              .position(latLng)
+	                              .draggable(true));
+	}
 
-		// Construct a CameraPosition focusing on Mountain View and animate the camera to that position.
-		/*CameraPosition cameraPosition = new CameraPosition.Builder()
-		    .target(MOUNTAIN_VIEW)      // Sets the center of the map to Mountain View
-		    .zoom(17)                   // Sets the zoom
-		    .bearing(90)                // Sets the orientation of the camera to east
-		    .tilt(30)                   // Sets the tilt of the camera to 30 degrees
-		    .build();                   // Creates a CameraPosition from the builder
-		map.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));*/
+	@Override
+	public void onProviderDisabled(String provider) {
+		// TODO Auto-generated method stub
 		
 	}
 
+	@Override
+	public void onProviderEnabled(String provider) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onStatusChanged(String provider, int status, Bundle extras) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onMarkerDrag(Marker arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@SuppressWarnings("deprecation")
+	@Override
+	public void onMarkerDragEnd(Marker marker) {
+		AlertDialog alertDialog = new AlertDialog.Builder(this).create();
+		alertDialog.setTitle("Is this your desired location?");
+		alertDialog.setMessage(String.valueOf(marker.getPosition()));
+		alertDialog.setButton("OK", new DialogInterface.OnClickListener() {
+		   public void onClick(DialogInterface dialog, int which) {
+		      // TODO Add your code for the button here.
+		   }
+		});
+		// Set the Icon for the Dialog
+		alertDialog.show();
+		
+		
+		
+	}
+
+	@Override
+	public void onMarkerDragStart(Marker arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
 }
